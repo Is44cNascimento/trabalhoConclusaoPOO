82f27440074ed9ff8a574587056627199a0e9bba
Is44cNascimento <isaacgamex7@outlook.com>
2025-05-21 19:16:51 -0300

    subindo inicio

--- src/Acessorio.java
+++ src/Acessorio.java
@@ -1,2 +1,27 @@
-public class Acessorio {
+public class Acessorio extends Peca {
+
+    public Acessorio(String descricao, int quantidade, int estoqueMaximo, int estoqueMinimo) {
+        super(descricao, quantidade, estoqueMaximo, estoqueMinimo);
+    }
+
+    @Override
+    public void venda(int quantidade) {
+        if(getQuantidade() > quantidade) {
+            setQuantidade(getQuantidade() - quantidade);
+        }
+        else {
+        System.out.println("QUANTIDADE INSUFICIENTE");
+        }
+    }
+
+    @Override
+    public void reposicaoEstoque() {
+        if (getQuantidade() < getEstoqueMinimo()){
+            setQuantidade(getEstoqueMaximo());
+        }
+        else {
+            System.out.println("ESTOQUE ACIMA DA QUANTIDADE MINIMA");
+        }
+
+    }
 }
--- src/Peca.java
+++ src/Peca.java
@@ -1,2 +1,50 @@
-public class Peca {
+public abstract class Peca {
+    private String descricao;
+    private int quantidade;
+    private int estoqueMinimo;
+    private int estoqueMaximo;
+
+    public Peca(String descricao, int quantidade, int estoqueMaximo, int estoqueMinimo) {
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.estoqueMaximo = estoqueMaximo;
+        this.estoqueMinimo = estoqueMinimo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getEstoqueMinimo() {
+        return estoqueMinimo;
+    }
+
+    public void setEstoqueMinimo(int estoqueMinimo) {
+        this.estoqueMinimo = estoqueMinimo;
+    }
+
+    public int getEstoqueMaximo() {
+        return estoqueMaximo;
+    }
+
+    public void setEstoqueMaximo(int estoqueMaximo) {
+        this.estoqueMaximo = estoqueMaximo;
+    }
+
+    public abstract void venda(int quantidade);
+    public abstract void reposicaoEstoque();
+
 }
+
